e530d0c2cfc30db27838bc90c61cbe18b0caeef4
Bluemo <shutaro.aoyama@gmail.com>
2023-06-03 23:57:30 -0400

    見返せる仕組み実装, still flickering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react';
+import { useInterval } from 'usehooks-ts';
 
 function App() {
+  const [recording, setRecording] = useState(false);
+  const [recordedChunks, setRecordedChunks] = useState([]);
+  const [isPlaying, setPlaying] = useState(false);
+  const recordedVideoRef = useRef();
+  let mediaRecorder = useRef(null);
+
+  const handleStart = async () => {
+    const stream = await navigator.mediaDevices.getDisplayMedia({
+      video: {
+        cursor: 'always'
+      },
+      audio: false
+    });
+
+    mediaRecorder.current = new MediaRecorder(stream);
+    mediaRecorder.current.ondataavailable = handleDataAvailable;
+    mediaRecorder.current.start(100); // collect 100ms of data at a time
+    setRecording(true);
+    setPlaying(true);
+  };
+
+  const handleDataAvailable = (event) => {
+    if (event.data && event.data.size > 0) {
+      setRecordedChunks((prev) => [...prev, event.data]);
+    }
+  };
+
+  const handleStop = () => {
+    mediaRecorder.current.stop();
+    mediaRecorder.current.stream.getTracks().forEach((track) => track.stop());
+    setRecording(false);
+    setPlaying(false);
+  };
+
+  useInterval(() => {
+    let blob = new Blob(recordedChunks, { type: 'video/webm' });
+    let url = URL.createObjectURL(blob);
+    let currentTime = recordedVideoRef.current.currentTime;
+    recordedVideoRef.current.src = url;
+    recordedVideoRef.current.currentTime = currentTime;
+  }, isPlaying ? 5000 : null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Screen Recorder</h1>
+      <button onClick={recording ? handleStop : handleStart}>
+        {recording ? 'Stop Recording' : 'Start Recording'}
+      </button>
+      <h2>Playback</h2>
+      <video ref={recordedVideoRef} style={{"maxWidth": "50%"}} autoPlay controls></video>
     </div>
   );
 }
